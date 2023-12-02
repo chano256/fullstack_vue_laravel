743b1ab5a2111a49900b46a523f5ecb82f1d4769
Allan Ochan <allanochan@yahoo.com>
2023-12-02 19:52:38 +0300

    adds other routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,16 +3,14 @@ import AuthorView from "../views/Author/AuthorView.vue";
 import AuthorListView from "../views/Author/AuthorView.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "authors",
-        component: AuthorListView,
-    },
-    {
-        path: "/:id",
-        name: "author",
-        component: AuthorView,
-    }
+    { path: "/create", name: "authors-create", component: AuthorView },
+    { path: "/",name: "authors-index",component: AuthorListView },
+    { path: "/:id", name: "authors-show", component: AuthorView },
+    { path: "/:id/update", name: "authors-update", component: AuthorView },
+    { path: "/:id/delete", name: "authors-delete", component: AuthorView },
+
+    // otherwise return them home
+    { path: "*", redirect: "/" }
 ];
 
 export default createRouter({
