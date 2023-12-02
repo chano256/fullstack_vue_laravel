eea9b00961a776eb70418f7043f893d2e6b059ae
Allan Ochan <allanochan@yahoo.com>
2023-12-02 19:52:31 +0300

    adds other routes

--- frontend/src/views/Author/AuthorListView.vue
+++ frontend/src/views/Author/AuthorListView.vue
@@ -19,7 +19,7 @@
 import axios from 'axios'
 
 export default {
-    name: 'Author',
+    name: 'author',
     data() {
         return {
             author: []
@@ -40,6 +40,27 @@ export default {
                     console.log(error)
                 })
             console.log('Get Author')
+        },
+        async getAuthors() {
+            await this.axios.get('/api/authors')
+                .then((response) => {
+                    console.log(response)
+                    this.author = response 
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            console.log('Get Author')
+        },
+        deleteAuthor(id){
+            if(confirm("Are you sure to delete this author ?")){
+                this.axios.delete(`/api/authors/${id}`)
+                    .then(response => {
+                        this.getCategories()
+                    }).catch(error=>{
+                        console.log(error)
+                    })
+            }
         }
     }
 }
