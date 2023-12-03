3a92e829d60840fc1a000738aa42c5c1a63494e0
Allan Ochan <allanochan@yahoo.com>
2023-12-03 07:39:05 +0300

    adds watch for changes in input

--- frontend/src/views/Author/Create.vue
+++ frontend/src/views/Author/Create.vue
@@ -8,6 +8,7 @@
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
                     <input type="text" class="form-control" v-model="author.name" />
+                    <p>{{ author.name }}</p>
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email</label>
@@ -37,7 +38,6 @@ export default {
     },
     mounted() {
         this.getAuthors()
-        console.log('Authors create mounted')
     },
     methods: {
         getAuthors() {
@@ -49,6 +49,14 @@ export default {
                     console.log(error)
                 })
         },
+    },
+    watch: {
+        // watches for changes in name input
+        'author.name'(newName) {
+            if (newName.length > 3) {
+                console.log('Name changed:', newName);   
+            }
+        }
     }
 }
 </script>
