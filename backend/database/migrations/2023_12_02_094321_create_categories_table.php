d1ab9bacee23fea8ccd85478f95446bf337cbac9
Allan Ochan <allanochan@yahoo.com>
2023-12-03 13:56:30 +0300

    increases slug field

--- backend/database/migrations/2023_12_02_094321_create_categories_table.php
+++ backend/database/migrations/2023_12_02_094321_create_categories_table.php
@@ -15,8 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 20);
-            $table->string('slug', 50)->unique();
+            $table->string('name', 80);
+            $table->string('slug', 80)->unique();
             $table->string('description');
         });
     }
